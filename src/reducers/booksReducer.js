5dd40442d387c3d0cbf7e7d80f30df3f5701a7a7
bmanek <41875460+bmanek@users.noreply.github.com>
2020-06-23 10:49:59 -0400

    connect BookList to redux

--- src/reducers/booksReducer.js
+++ src/reducers/booksReducer.js
@@ -1,9 +1,9 @@
 export default function() {
   return [
-    title: "Learn Python the Hard Way",
-    title: "Cracking the Coding Interview",
-    title: "As I Lay Dying",
-    title: "Everything I Never Told You",
-    title: "Megafire",
+    {title: "Learn Python the Hard Way"},
+    {title: "Cracking the Coding Interview"},
+    {title: "As I Lay Dying"},
+    {title: "Everything I Never Told You"},
+    {title: "Megafire"}
   ]
 }
