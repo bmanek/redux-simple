93dbab22778f2d85f478384742ce351d83c2a348
bmanek <41875460+bmanek@users.noreply.github.com>
2020-06-23 10:25:03 -0400

    add booksReducer

---
+++ src/reducers/booksReducer.js
@@ -0,0 +1,9 @@
+export default function() {
+  return [
+    title: "Learn Python the Hard Way",
+    title: "Cracking the Coding Interview",
+    title: "As I Lay Dying",
+    title: "Everything I Never Told You",
+    title: "Megafire",
+  ]
+}
