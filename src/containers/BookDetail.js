abb127be283ef497ebf1879f2693feeccbe7ebe4
bmanek <41875460+bmanek@users.noreply.github.com>
2020-06-23 13:37:53 -0400

    add conditional rendering

--- src/containers/BookDetail.js
+++ src/containers/BookDetail.js
@@ -3,11 +3,26 @@ import { connect } from 'react-redux'
 
 class BookDetail extends Component {
   render() {
+    if (!this.props.book) {
+      return (
+        <div>Select a book to get started</div>
+      )
+    }
+
+
     return (
-      <div>Book Detail</div>
+      <div>
+        <h3>Details for:</h3>
+        <div>Title: {this.props.book.title}</div>
+      </div>
     )
   }
 }
+
+// Below used to be ln 17. Removed b/c it crashed, "cannot read property
+// 'pages' of undefined"
+// No idea why, conditional render works fine w/ just Title
+// <div>Pages: {this.props.books.pages}</div>
 
 function mapStateToProps(state) {
   return {
--- src/reducers/booksReducer.js
+++ src/reducers/booksReducer.js
@@ -1,9 +1,9 @@
 export default function() {
   return [
-    {title: "Learn Python the Hard Way"},
-    {title: "Cracking the Coding Interview"},
-    {title: "As I Lay Dying"},
-    {title: "Everything I Never Told You"},
-    {title: "Megafire"}
+    {title: "Learn Python the Hard Way", pages:89 },
+    {title: "Cracking the Coding Interview", pages: 603 },
+    {title: "As I Lay Dying", pages: 250 },
+    {title: "Everything I Never Told You", pages: 243 },
+    {title: "Megafire", pages: 435 }
   ]
 }
