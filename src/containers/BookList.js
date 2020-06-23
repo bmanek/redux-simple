a5c4153d6241551c3e5f12905625d19f02faaf66
bmanek <41875460+bmanek@users.noreply.github.com>
2020-06-23 13:20:00 -0400

    add BookDetail container

--- src/containers/BookList.js
+++ src/containers/BookList.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { selectBook } from '../actions/index'
+import { bindActionCreators } from 'redux'
 
 class BookList extends Component {
   renderList() {
     return(this.props.books.map((book) => {
       return(
-        <li key={book.title} className="list-group-item">{book.title}</li>
+        <li
+          key={book.title}
+          onClick={ () => this.props.selectBook(book)}
+          className="list-group-item">
+          {book.title}
+        </li>
       )
     }))
   }
@@ -19,10 +26,20 @@ class BookList extends Component {
   }
 }
 
+
+// Anything returned from this function will end up as props on the BookList
+// container
+function mapDispatchToProps(dispatch) {
+  // whenever selectBook is called, the result should be passed to all reducers
+  return bindActionCreators({ selectBook: selectBook }, dispatch)
+}
+
 function mapStateToProps(state) {
   return {
     books: state.books
   }
 }
 
-export default connect(mapStateToProps)(BookList)
+// syntax below promotes BookList to container - it needs to know about the
+// selectBook method and how it's dispatched. Make it available as a prop
+export default connect(mapStateToProps, mapDispatchToProps)(BookList)
---
+++ src/reducers/activeBookReducer.js
@@ -0,0 +1,12 @@
+// state here is not application state, but just the piece this reducer
+// is responsible for
+
+export default function(state = null, action) {
+  switch(action.type) {
+    case 'BOOK_SELECTED':
+      return action.payload
+  }
+
+  return state
+
+}
