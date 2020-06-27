07b1b982856e2a7cdfef86d8dcfcf387a2ef8190
bmanek <41875460+bmanek@users.noreply.github.com>
2020-06-26 20:33:04 -0400

    add pages to BookDetail render

--- src/reducers/booksReducer.js
+++ src/reducers/booksReducer.js
@@ -1,6 +1,6 @@
 export default function() {
   return [
-    {title: "Learn Python the Hard Way", pages:89 },
+    {title: "Learn Python the Hard Way", pages: 89 },
     {title: "Cracking the Coding Interview", pages: 603 },
     {title: "As I Lay Dying", pages: 250 },
     {title: "Everything I Never Told You", pages: 243 },
